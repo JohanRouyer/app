e058de37779f617e5c92f84192f4ebf95de8a95c
johan ROUYER <johanrouyer2@gmail.com>
2023-09-26 09:21:33 +0700

    update

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: "sql.freedb.tech",
     USER: "freedb_johan-api",
-    PASSWORD: "@N3WdqbhE6CfRUe",
-    DB: "freedb_account management api"
+    PASSWORD: "F%@@w?%AzRvQ6Tv",
+    DB: "freedb_account-management-api"
     };
