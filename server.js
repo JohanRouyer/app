833e5a26bea4e9f36986522f75a03f8066bbfc3e
johan ROUYER <johanrouyer2@gmail.com>
2023-09-26 11:37:18 +0700

    up

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+
 global.__basedir = __dirname;
 var corsOptions = {
 origin: "*"
 };
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
